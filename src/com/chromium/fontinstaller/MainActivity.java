fcdb7905147bf49cbb0a2cf8509840bb04fb3e8c
Chromium1 <priyesh.96@hotmail.com>
2014-04-19 11:09:30 -0400

    Complete redesign of MainActivity with new Card UI

--- src/com/chromium/fontinstaller/MainActivity.java
+++ src/com/chromium/fontinstaller/MainActivity.java
@@ -23,21 +23,27 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.main_card);
 		prefs = getSharedPreferences("com.mycompany.myAppName", MODE_PRIVATE);
 
-		openFontList = (Button)findViewById(R.id.openFontList);
+		openFontList = (Button)findViewById(R.id.installFont);
 		openFontList.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v){
+				if(v == openFontList) {
+					openFontList.setBackgroundResource(R.drawable.layer_card_background_pressed);
+				}
 				Intent fontListActivity = new Intent(MainActivity.this, FontList.class);
 				startActivity(fontListActivity);
 
 			}
 		});
 
-		cleanFontDir = (Button)findViewById(R.id.cleanFontDir);
+		cleanFontDir = (Button)findViewById(R.id.deleteCache);
 		cleanFontDir.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v){
+				if(v == cleanFontDir) {
+					cleanFontDir.setBackgroundResource(R.drawable.layer_card_background_pressed);
+				}
 				File file = new File("/sdcard/DownloadedFonts");
 
 				if (file.exists()) {
@@ -88,14 +94,14 @@ public class MainActivity extends ActionBarActivity {
 			prefs.edit().putBoolean("firstrun", false).commit();
 		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem menuItem) {
 		if (menuItem.getItemId() == R.id.menu_about) { //about button in actionbar
