909fada0def314f8717354e5728e6e6fb8e4ce88
ItsPriyesh <priyesh.96@hotmail.com>
2015-10-20 22:09:15 -0700

    Setup animation for splash

--- app/src/main/java/com/chromium/fontinstaller/ui/splash/SplashActivity.java
+++ app/src/main/java/com/chromium/fontinstaller/ui/splash/SplashActivity.java
@@ -16,32 +16,79 @@
 
 package com.chromium.fontinstaller.ui.splash;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.chromium.fontinstaller.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class SplashActivity extends AppCompatActivity {
+
+    @Bind(R.id.splash_logo)
+    ImageView mSplashLogo;
+
+    @Bind(R.id.title)
+    TextView mTitleView;
+
+    @Bind(R.id.progress_circle)
+    ProgressBar mProgressCircle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        final View parent = findViewById(R.id.parent);
+        final Interpolator interpolator = new AccelerateDecelerateInterpolator();
+
+        mTitleView.setTypeface(Typeface.createFromAsset(getAssets(), "Quicksand-Regular.ttf"));
+
+        mProgressCircle.setScaleX(0);
+        mProgressCircle.setScaleY(0);
+        mProgressCircle.setVisibility(View.VISIBLE);
+        mProgressCircle.getIndeterminateDrawable()
+                .setColorFilter(Color.WHITE, PorterDuff.Mode.MULTIPLY);
+
+        mSplashLogo.setScaleX(0);
+        mSplashLogo.setScaleY(0);
+        mSplashLogo.setRotation(-1080);
+        mSplashLogo.setVisibility(View.VISIBLE);
+
+        parent.postDelayed(() -> mSplashLogo.animate()
+                .scaleX(1)
+                .scaleY(1)
+                .rotation(0)
+                .setDuration(1000)
+                .setInterpolator(interpolator)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        mTitleView.animate()
+                                .alpha(1)
+                                .setDuration(200)
+                                .setInterpolator(interpolator);
+                        mProgressCircle.animate()
+                                .scaleX(1)
+                                .scaleY(1)
+                                .setDuration(200)
+                                .setInterpolator(interpolator);
+                    }
+                }), 400);
     }
 
 }
